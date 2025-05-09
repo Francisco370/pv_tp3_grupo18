06a6a98eabd81c4b721f0b56e4db3e45c8b5d431
Francisco370 <ignacio2162001@gmail.com>
2025-05-09 20:58:46 -0300

    se realizaron algunos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,40 @@ import TaskList from './components/TaskList';
 import TaskInput from './components/TaskInput';
 
 function App() {
-  const[nidex, setIndex] = useState(1)
-  const[task, setTask] = useState([])
+  const [nidex, setIndex] = useState(1);
+  const [tasks, setTasks] = useState([]);
 
-     const addTask = (texto) => {
-     const newTask = {
-     id: nidex,
-     tarea_a_realizar: texto,
-     completada: false}
-     setTask([...task,newTask])
-     setIndex(nidex+1)
-    }
-    const toggleTask = (id) => {
-      const act = task.map(task => task.id=== id ? {...task, completada: !task.completada} : task
+  const addTask = (texto) => {
+    const newTask = {
+      id: nidex,
+      tarea_a_realizar: texto,
+      completada: false,
+    };
+    setTasks([...tasks, newTask]);
+    setIndex(nidex + 1);
+  };
+
+  const toggleTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completada: !task.completada } : task
     );
-      setTask(act)
-    }
-    const deleteTask = (id) => {
-      const filtro = task.filter(task => task.id !== id)
-      setTask(filtro)
-    }
-    return(
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
+  };
+
+  return (
     <div className="App">
-      <h1>Lista de tareas</h1>
-      <TaskInput addTask={addTask}/>
-      <TaskList task={task} toggleTask={toggleTask}  deleteTask={deleteTask}/>
+      <h1>Lista de Tareas</h1>
+      <TaskInput addTask={addTask} />
+      <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
     </div>
-    )
+  );
 }
+
   
   
 
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,22 +3,26 @@ import TaskInput from './TaskInput';
 import TaskItem from './TaskItem';
 import '../css/lista.css';
 
-const TaskList = ({task,toggleTask,deleteTask}) => {
-    return(
- <div id="idlista" >
-        <ul Classname='Lista de tareas'>
-            {task.map((task) => (
-                <TaskItem
-                    key={task.id}
-                    task={task}
-                    toggleTask={toggleTask}
-                    deleteTask={deleteTask}
-                />
-            ))}
+const TaskList = ({ tasks, toggleTask, deleteTask }) => {
+    return (
+      <div id="idlista">
+        <ul className='Lista-de-tareas'>
+          {tasks.length > 0 ? (
+            tasks.map((task) => (
+              <TaskItem
+                key={task.id}
+                task={task}
+                toggleTask={toggleTask}
+                deleteTask={deleteTask}
+              />
+            ))
+          ) : (
+            <li>No hay tareas aún.</li>
+          )}
         </ul>
- </div>
+      </div>
     );
-};
+  };
 
 export default TaskList;
 
